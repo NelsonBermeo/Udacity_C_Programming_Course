0df4c4cd4e2d6a991d8ed217706b7a7d2a1ff814
Nelson Bermeo <nelsonberm.2005@gmail.com>
2025-08-13 20:48:01 -0400

    Finished Variable File & Added Operator Folder

---
+++ Intro/operators/operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    float avg;
+    avg = (float) ((a+b+c) / 3);
+    printf("The avg is %.2f\n", avg);
+    return 0;
+
+}
